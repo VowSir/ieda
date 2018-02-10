d132f6ed7b12c15b13580f18a595b496c6944042
maangming <82566@163.com>
2018-02-10 13:52:19 +0800

    使用idea

--- consoles/.history/db/5589ea3f-f111-4238-8d8d-8f98dad4eb31.sql
+++ consoles/.history/db/5589ea3f-f111-4238-8d8d-8f98dad4eb31.sql
@@ -53,27 +53,3 @@ from T_OWNERTYPE ot ,T_OWNERS o,T_ADDRESS TA WHERE o.OWNERTYPEID = ot.ID and ta.
 SELECT  o.ID 业主编号, o.NAME 业主名称,ot.name 业主类型的名称
   ,ta.name 业主居住的地方,ar.name 业主所属区域
 from T_OWNERTYPE ot ,T_OWNERS o,T_ADDRESS TA,T_AREA ar WHERE o.OWNERTYPEID = ot.ID and ta.ID = o.ADDRESSID and ar.ID = o.ADDRESSID
-;-- -. . -..- - / . -. - .-. -.--
-SELECT  o.ID 业主编号, o.NAME 业主名称,ot.name 业主类型的名称
-  ,ta.name 业主居住的地方,ar.name 业主所属区域,tr.name 收费员的名称
-from T_OWNERTYPE ot ,T_OWNERS o,T_ADDRESS TA,T_AREA ar WHERE o.OWNERTYPEID = ot.ID and ta.ID = o.ADDRESSID and ar.ID = o.ADDRESSID and tr.id =ta.AREAID
-;-- -. . -..- - / . -. - .-. -.--
-SELECT  o.ID 业主编号, o.NAME 业主名称,ot.name 业主类型的名称
-  ,ta.name 业主居住的地方,ar.name 业主所属区域,tr.name 收费员的名称
-from T_OWNERTYPE ot ,T_OWNERS o,T_ADDRESS TA,T_AREA ar,T_OPERATOR tr  WHERE o.OWNERTYPEID = ot.ID and ta.ID = o.ADDRESSID and ar.ID = o.ADDRESSID and tr.id =ta.AREAID
-;-- -. . -..- - / . -. - .-. -.--
-SELECT * FROM T_ACCOUNT
-;-- -. . -..- - / . -. - .-. -.--
-SELECT OWNERUUIDNK 业主的编号,o.NAME 业主的名字,YEAR 年,MONTH 月,MONEY 金额  FROM T_ACCOUNT t LEFT JOIN T_OWNERS o WHERE t.OWNERUUID =o.ID
-;-- -. . -..- - / . -. - .-. -.--
-SELECT OWNERUUIDNK 业主的编号,o.NAME 业主的名字,YEAR 年,MONTH 月,MONEY 金额  FROM T_ACCOUNT t LEFT JOIN T_OWNERS o on t.OWNERUUID =o.ID
-;-- -. . -..- - / . -. - .-. -.--
-SELECT o.OWNERUUIDNK 业主的编号,o.NAME 业主的名字,o.YEAR 年,o.MONTH 月,o.MONEY 金额  FROM T_ACCOUNT t LEFT JOIN T_OWNERS o on t.OWNERUUID =o.ID
-;-- -. . -..- - / . -. - .-. -.--
-SELECT t.OWNERUUIDNK 业主的编号,o.NAME 业主的名字,t.YEAR 年,t.MONTH 月,t.MONEY 金额  FROM T_ACCOUNT t LEFT JOIN T_OWNERS o on t.OWNERUUID =o.ID
-;-- -. . -..- - / . -. - .-. -.--
-SELECT t.OWNERUUID 业主的编号,o.NAME 业主的名字,t.YEAR 年,t.MONTH 月,t.MONEY 金额  FROM T_ACCOUNT t LEFT JOIN T_OWNERS o on t.OWNERUUID =o.ID
-;-- -. . -..- - / . -. - .-. -.--
-SELECT t.OWNERUUID 业主的编号,o.NAME 业主的名字,t.YEAR 年,t.MONTH 月,t.MONEY 金额  FROM T_ACCOUNT t right JOIN T_OWNERS o on t.OWNERUUID =o.ID
-;-- -. . -..- - / . -. - .-. -.--
-select ow.id,ow.name,ac.year,ac.month,ac.money from T_OWNERS ow right join T_ACCOUNT ac on ow.id=ac.owneruuid
