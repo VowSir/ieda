1bf2fe24e0e2525c43f20a99291dcee699f6cbb1
maangming <82566@163.com>
2018-02-11 19:50:00 +0800

    无修改

--- fileTemplates/internal/Class.java
+++ fileTemplates/internal/Class.java
@@ -1,6 +1,11 @@
 #if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
 #parse("File Header.java")
-
+/**
+ * 描述:
+ *    ${DESCRIPTION}
+ * @outhor ${USER}
+ * @create ${YEAR}-${MONTH}-${DAY} ${TIME}
+ */
 public class ${NAME} {
     
 }
