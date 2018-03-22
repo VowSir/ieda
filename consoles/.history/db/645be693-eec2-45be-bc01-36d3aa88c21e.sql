c79015320183ebb9986ca41c8d5030cb7d28d0df
maangming <maangming@gmail.com>
2018-03-22 23:03:21 +0800

    重装系统

--- consoles/.history/db/645be693-eec2-45be-bc01-36d3aa88c21e.sql
+++ consoles/.history/db/645be693-eec2-45be-bc01-36d3aa88c21e.sql
@@ -1,7 +1,5 @@
 CREATE DATABASE sell
 ;-- -. . -..- - / . -. - .-. -.--
-USE sell
-;-- -. . -..- - / . -. - .-. -.--
 CREATE TABLE `product_category` (
   `category_id` INT NOT NULL AUTO_INCREMENT,
   `category_name` VARCHAR(64) NOT NULL COMMENT '类目名字',
@@ -10,3 +8,16 @@ CREATE TABLE `product_category` (
   `update_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
   PRIMARY KEY (`category_id`)
 )
+;-- -. . -..- - / . -. - .-. -.--
+USE sell
+;-- -. . -..- - / . -. - .-. -.--
+create table `product_category` (
+  `category_id` int not null auto_increment,
+  `category_name` varchar(64) not null comment '类目名字',
+  `category_type` int not null comment '类目编号',
+  `create_time` timestamp not null default current_timestamp comment '创建时间',
+  `update_time` timestamp not null default current_timestamp on update current_timestamp comment '修改时间',
+  primary key (`category_id`)
+)
+;-- -. . -..- - / . -. - .-. -.--
+SHOW DATABASES
--- consoles/db/645be693-eec2-45be-bc01-36d3aa88c21e/console.sql
+++ consoles/db/645be693-eec2-45be-bc01-36d3aa88c21e/console.sql
@@ -1,12 +1,14 @@
 CREATE DATABASE sell;
 USE sell;
-CREATE TABLE `product_category` (
-  `category_id` INT NOT NULL AUTO_INCREMENT,
-  `category_name` VARCHAR(64) NOT NULL COMMENT '类目名字',
-  `category_type` INT NOT NULL COMMENT '类目编号',
-  `create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `update_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-  PRIMARY KEY (`category_id`)
+-- 类目
+create table `product_category` (
+  `category_id` int not null auto_increment,
+  `category_name` varchar(64) not null comment '类目名字',
+  `category_type` int not null comment '类目编号',
+  `create_time` timestamp not null default current_timestamp comment '创建时间',
+  `update_time` timestamp not null default current_timestamp on update current_timestamp comment '修改时间',
+  primary key (`category_id`)
 );
+SHOW DATABASES ;
 
 
